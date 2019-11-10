8a566b16eabfd598023bc7a3c63c9cb87b802bc1
LittleInoriai <Inoriai@126.com>
2019-11-10 21:11:34 +0800

    make a authorize page

---
+++ mini-app/pages/authorize/authorize.js
@@ -0,0 +1,121 @@
+// pages/authorize/authorize.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //判断小程序的API，回调，参数，组件等是否在当前版本可用。
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 查看是否授权
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              console.log(res)
+              //获取用户临时code
+              wx.login({
+                success: res => {
+                  console.log(res)
+                }
+              })
+              //从数据库获取用户信息
+              
+              //用户已经授权过但并未绑定信息
+              wx.redirectTo({
+                url: '/pages/bind/bind',
+              })
+              //用户已经授权且已绑定信息
+              //wx.redirectTo({
+              //  url: '/pages/index/index',
+              //})
+            }
+          });
+        }
+      }
+    })
+  },
+
+  getUserinfo: function(e) {
+    console.log(e.detail)
+    if (e.detail.userInfo) {
+      //用户按了允许授权按钮
+      //插入登录的用户的相关信息到数据库
+      wx.request({
+        
+      });
+      //授权成功后，跳转进入小程序首页
+      wx.redirectTo({
+        url: '/pages/bind/bind',
+      })
+    } else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '警告',
+        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+        showCancel: false,
+        confirmText: '返回授权',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击了“返回授权”')
+          }
+        }
+      })
+    }
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
