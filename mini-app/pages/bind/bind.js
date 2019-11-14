f90fad7d37e79cd5b09cd6a003c737b074c38ce1
LittleInoriai <Inoriai@126.com>
2019-11-14 19:13:24 +0800

    create a new branch

---
+++ mini-app/pages/bind/bind.js
@@ -0,0 +1,76 @@
+// pages/bind/bind.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    //判断学号和姓名是否存在
+    //若不存在，返回toast提示信息
+    //若存在顺便判断是否已绑定微信
+    //若已绑定，返回toast提示信息
+    //判断全部通过后，更新数据库，将用户信息与微信openid绑定
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    //隐藏返回第一个页面按钮
+    wx.hideHomeButton()
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
