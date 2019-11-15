c7e9f815d70f0c449841101674a96baa7d8e6100
DannyDiao <diaosu@diaosudev.cn>
2019-11-15 22:18:01 +0800

    Init Spring Boot.

---
+++ back-end/src/test/java/com/diaosu/quickfile/QuickfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diaosu.quickfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
