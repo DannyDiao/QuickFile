1dfe41ee234512d2c1a14bc3fe01be0772a03d0b
diaosu <diaosu@diaosudev.cn>
2020-04-06 02:55:44 +0800

    prepare annotations.

---
+++ back-end/src/main/java/com/diaosu/quickfile/annotations/PassToken.java
@@ -0,0 +1,15 @@
+package com.diaosu.quickfile.annotations;
+/* Created By DiaoSu
+   Date  2020/4/6
+   Mail  diaosu@diaosudev.cn */
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PassToken {
+    boolean required() default true;
+}
